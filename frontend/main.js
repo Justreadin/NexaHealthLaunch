e35370272f67ed9e3d6d9b21c41b49f6c037e45b
Justreadin <dave.400g@gmail.com>
2025-07-06 08:29:23 +0100

    added default host

--- frontend/main.js
+++ frontend/main.js
@@ -106,7 +106,7 @@ if (betaForm) {
                 timestamp: new Date().toISOString()
             };
             
-            const response = await fetch('http://127.0.0.1:8000/api/submissions', {
+            const response = await fetch('https://lyrecal.onrender.com/api/submissions', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
