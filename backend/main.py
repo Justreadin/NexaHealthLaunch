dedb40b854dd1b1da91d2b72f0113a16727c5264
Justreadin <dave.400g@gmail.com>
2025-07-06 08:31:19 +0100

    added default host

--- backend/main.py
+++ backend/main.py
@@ -29,10 +29,17 @@ db = firestore.client()
 
 app = FastAPI()
 
-# CORS configuration
+# CORS Configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:63342",
+        "https://nexa-health-page.vercel.app",
+        "http://localhost:3000",
+        "http://localhost:8000",
+        "http://localhost:5500",
+        "http://127.0.0.1:5500"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
